b62c25fe0cb251bdf395582220a9de3c87b6c01b
Mohit <mohitmayank26@gmail.com>
2023-04-10 17:40:59 +0530

    Adding API

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from home.views import BlogView,PublicBlog
+urlpatterns = [
+    path('blog/' , BlogView.as_view()),
+    path('',PublicBlog.as_view())
+   
+]
---
+++ home/views.py
@@ -0,0 +1,191 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import BlogSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.core.paginator import Paginator
+from .models import Blog
+from django.db.models import Q
+# Create your views here.
+
+
+class PublicBlog(APIView):
+
+    def get(self ,request):
+
+        try:
+
+            blogs=Blog.objects.all().order_by('?')  #fetch random post
+            print(blogs)
+            if request.GET.get('search'):
+                search = request.GET.get('search')
+                blogs=blogs.filter(Q(title__icontains = search) | Q(blog_text__icontains=search) )
+            
+            page_number = request.GET.get('page' ,1)
+            paginator = Paginator(blogs ,1)
+
+            serializer=BlogSerializer(paginator.page(page_number) , many=True)
+            return Response({
+                   'data':serializer.data,
+                   'message':"blog fetched successfully",
+                  
+               },status=status.HTTP_200_OK)
+             
+        except Exception as e:
+            print(e)
+            return Response({
+                "data":{},
+                "message":"something went wrong or invalied page"
+
+            },status=status.HTTP_400_BAD_REQUEST) 
+
+
+
+
+
+class BlogView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes=[JWTAuthentication]
+    
+    
+    def get(self,request):
+
+        try:
+            blogs=Blog.objects.filter(user=request.user)
+            print(blogs)
+            if request.GET.get('search'):
+                search = request.GET.get('search')
+                blogs=blogs.filter(Q(title__icontains = search) | Q(blog_text__icontains=search) )
+            
+            serializer=BlogSerializer(blogs , many=True)
+            return Response({
+                   'data':serializer.data,
+                   'message':"blog fetched successfully",
+                  
+               },status=status.HTTP_200_OK)
+             
+        except Exception as e:
+            print(e)
+            return Response({
+                "data":{},
+                "message":"something went wrong"
+
+            },status=status.HTTP_400_BAD_REQUEST) 
+
+
+    def post(self,request):
+        try:
+            data=request.data 
+            print(request.user)
+            data['user']=request.user.id
+            serializer=BlogSerializer(data=data)
+
+            if not serializer.is_valid():
+               return Response({
+                   'data':serializer.errors,
+                   'message':"something went wrong",
+                  
+               },status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer.save()
+            return Response({
+                   'data':serializer.data,
+                   'message':"blog created successfully",
+                  
+               },status=status.HTTP_201_CREATED)
+               
+           
+            
+        except Exception as e:
+            print(e)
+            return Response({
+                "data":{},
+                "message":"something went wrong"
+
+            },status=status.HTTP_400_BAD_REQUEST) 
+
+    def patch(self,request):
+
+        try:    
+            data=request.data 
+            blog=Blog.objects.filter(uid=data.get('uid'))
+           
+            if not blog.exists():
+                return Response({
+                "data":{},
+                "message":"Does not exist"
+
+            },status=status.HTTP_400_BAD_REQUEST) 
+
+           
+
+            if request.user != blog[0].user:
+                return Response({
+                "data":{},
+                "message":"You are not authorized to this"
+
+            },status=status.HTTP_400_BAD_REQUEST) 
+
+
+            serializer=BlogSerializer(blog[0], data=data , partial=True)
+  
+            if not serializer.is_valid():
+               return Response({
+                   'data':serializer.errors,
+                   'message':"something went wrong",
+                  
+               },status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer.save()
+            return Response({
+                   'data':serializer.data,
+                   'message':"blog Updated successfully",
+                  
+               },status=status.HTTP_201_CREATED)
+        except Exception as e:
+            print(e)
+            return Response({
+                "data":{},
+                "message":"something went wrong"
+
+            },status=status.HTTP_400_BAD_REQUEST)        
+           
+            
+    def delete(self , request):
+        try:    
+            data=request.data 
+            blog=Blog.objects.filter(uid=data.get('uid'))
+           
+            if not blog.exists():
+                return Response({
+                "data":{},
+                "message":"Does not exist"
+
+            },status=status.HTTP_400_BAD_REQUEST) 
+
+           
+
+            if request.user != blog[0].user:
+                return Response({
+                "data":{},
+                "message":"You are not authorized to this"
+
+            },status=status.HTTP_400_BAD_REQUEST)
+
+            blog[0].delete()
+
+            return Response({
+                   'data':{},
+                   'message':"blog deleted successfully",
+                  
+               },status=status.HTTP_201_CREATED)
+        
+        except Exception as e:
+            print(e)
+            return Response({
+                "data":{},
+                "message":"something went wrong"
+
+            },status=status.HTTP_400_BAD_REQUEST)        
+                    
